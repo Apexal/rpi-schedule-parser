ca10a98b27b9fb5dcc4a1424b3253b0569eb15d0
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-06 21:25:04 -0400

    Comment every property on interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,30 +1,46 @@
 /** Represents a single course **for a specific term**. */
 export interface Course {
+  /** The unique identifier for a term made from combining start year with start month, e.g. `"202001"`. */
   termCode: string;
+  /** The title of the course. */
   title: string;
+  /** The subject the course falls under, e.g. `"BIOL"` or `"CSCI"`. */
   subjectPrefix: string;
+  /** The number of the course, e.g. `"1010"` from `"BIOL 1010"`. */
   subjectCode: string;
+  /** The different sections for this course. */
   sections?: Section[];
   [key: string]: any;
 }
 
 /** Represents a single section for a course with a unique `crn` and `section`. Contains multiple periods. */
 export interface Section {
+  /** Unique identifier for this course section in a certain term. May repeat over different terms. */
   crn: string;
+  /** Section code for this section, e.g. `"01"` or `"07"` */
   section: string;
+  /** The periods associated with this section. */
   periods?: Period[];
   [key: string]: any;
 }
 
 /** Represents a single weekly period associated with a section of a course. Might meet multiple days of the week. */
 export interface Period {
-  type: string;
+  /** The type of period, e.g. lecture, test block, recitation, etc. */
+  type: "LEC" | "STU" | "TES" | "SEM" | "LAB" | "REC";
+  /** Starting time of period in HH:mm (0-padded, 24-hour format). */
   startTime: string;
+  /** Ending time of period in HH:mm (0-padded, 24-hour format) in EST. */
   endTime: string;
+  /** Days that this period takes place each week (0 is Sunday) in EST. */
   days: number[];
+  /** Date the period starts occurring in YYYY-MM-DD format in EST. */
   startDate?: string;
+  /** Date the period stops occurring in YYYY-MM-DD format in EST */
   endDate?: string;
+  /** Where this period takes place in BUILDING ROOM# format, e.g. `"DARRIN 308"`. */
   location: string;
+  /** The instructors associated with this period. Just last names. */
   instructors: string[];
   [key: string]: any;
 }
