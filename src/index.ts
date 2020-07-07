d05a5d841a831febf1dba0c35a08aef945e21737
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-06 22:22:54 -0400

    Export main functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+import { getScheduleDocumentForTerm, getPeriods } from "./scraping";
+import { compileCourses } from "./utils";
 
+/**
+ * Get all separate periods for a specific school term.
+ *
+ * @param termCode The code for the term, e.g. `"202001"`
+ * @returns Promise containing parsed periods
+ */
+export async function getPeriodsForTerm(termCode: string) {
+  const document = await getScheduleDocumentForTerm(termCode);
+  return getPeriods(document, termCode);
+}
+
+/**
+ * Get all courses, their sections, and their sections' periods for a specific school term.
+ *
+ * @param termCode The code for the term, e.g. `"202001"`
+ * @returns Promise containing parsed courses with section and periods
+ */
+export async function getCoursesForTerm(termCode: string) {
+  const periods = await getPeriodsForTerm(termCode);
+  return compileCourses(periods, termCode);
+}
